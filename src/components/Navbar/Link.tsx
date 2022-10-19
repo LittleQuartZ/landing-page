c46064598f2afdfa424e31228e8485f859d3fb91
Syahdan <58659387+LittleQuartZ@users.noreply.github.com>
2022-10-19 22:48:08 +0700

    navbar responsive

--- src/components/Navbar/Link.tsx
+++ src/components/Navbar/Link.tsx
@@ -9,7 +9,7 @@ const Link = ({ nav }: Props) => {
   return (
     <a
       href={nav.link}
-      className="first:border-t-2 hover:font-bold outline-none focus:font-bold focus:underline transition-all hover:underline hover:scale-105 active:scale-90 py-4 w-full text-center border-b-2 border-gray-100"
+      className="md:border-0 md:mr-6 md:first:border-0 first:border-t-2 hover:font-bold outline-none focus:font-bold focus:underline transition-all hover:underline hover:scale-105 active:scale-90 py-4 w-full text-center border-b-2 border-gray-100"
     >
       {nav.text}
     </a>
--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -13,10 +13,13 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`shadow bg-white grid grid-cols-2 items-center z-20 sticky top-0`}
+      className={`shadow bg-white grid grid-cols-2 items-center z-20 sticky top-0 md:container md:mx-auto`}
     >
       <Heading title={title} />
-      <button className="ml-auto p-6" onClick={() => setOpened(!opened)}>
+      <button
+        className="ml-auto p-6 md:hidden"
+        onClick={() => setOpened(!opened)}
+      >
         {opened ? (
           <RiCloseLine className="text-2xl" />
         ) : (
@@ -24,16 +27,18 @@ const Navbar = () => {
         )}
       </button>
 
-      {opened && (
-        <section className="flex flex-col col-span-2 justify-center absolute top-full items-center shadow-md w-full z-10 bg-white">
-          {navigations.map((nav) => (
-            <Link key={nav.id} nav={nav} />
-          ))}
-          <button className="hover:shadow-lg active:scale-95 font-bold text-green-700 transition-all py-4 w-[95%] text-center bg-green-300 my-4 rounded">
-            Register Now!
-          </button>
-        </section>
-      )}
+      <section
+        className={`md:!inline md:static md:w-max md:ml-auto md:mr-4 flex flex-col justify-center absolute top-full items-center md:shadow-none shadow-md w-full z-10 bg-white ${
+          !opened && "!hidden"
+        }`}
+      >
+        {navigations.map((nav) => (
+          <Link key={nav.id} nav={nav} />
+        ))}
+        <button className="hover:shadow-lg active:scale-95 font-bold text-green-700 transition-all md:px-4 py-4 w-[95%] md:w-max text-center bg-green-300 my-4 rounded">
+          Register Now!
+        </button>
+      </section>
     </nav>
   );
 };
